e01a56f2b300e7e704d9f9d8d1fdde13b7413077
Sa1Raz <rayazroblox@gmail.com>
2025-11-20 19:54:57 +0300

    Update socket-client.js

--- public/socket-client.js
+++ public/socket-client.js
@@ -10,12 +10,12 @@
 
   socket.on('connect', () => console.log('socket connected', socket.id));
   socket.on('connect_error', (err) => console.error('socket connect_error', err));
-  socket.on('new_topup', (data) => { console.log('new_topup', data); if(typeof loadTopups === 'function') loadTopups(); });
-  socket.on('update_topup', (data) => { console.log('update_topup', data); if(typeof loadTopups === 'function') loadTopups(); });
-  socket.on('new_withdraw', (data) => { console.log('new_withdraw', data); if(typeof loadWithdraws === 'function') loadWithdraws(); });
-  socket.on('new_work', (data) => { console.log('new_work', data); if(typeof loadWorks === 'function') loadWorks(); });
-  socket.on('new_topup_user', (data) => { alert('Заявка на пополнение принята'); try{ if(typeof renderMyRequests === 'function') renderMyRequests(); }catch(e){} });
-  socket.on('new_work_user', (data) => { alert('Ваша заявка принята на проверку'); try{ if(typeof renderMyRequests === 'function') renderMyRequests(); }catch(e){} });
+  socket.on('new_topup', (data) => { console.log('new_topup', data); });
+  socket.on('update_topup', (data) => { console.log('update_topup', data); });
+  socket.on('new_withdraw', (data) => { console.log('new_withdraw', data); });
+  socket.on('new_work', (data) => { console.log('new_work', data); });
+  socket.on('new_topup_user', (data) => { alert('Заявка на пополнение принята'); });
+  socket.on('new_work_user', (data) => { alert('Ваша заявка принята на проверку'); });
   socket.on('disconnect', (reason) => console.log('socket disconnected', reason));
 
   window.__RC_SOCKET = socket;
